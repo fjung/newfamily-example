aff87d646ca2b83187270f2f163c30f2654fbcb8
Frederik Jung <Frederik.Jung@mcl.de>
2016-03-28 18:40:45 +0200

    update angular to 2.0.0-beta.12

--- client/app/bootstrap.ts
+++ client/app/bootstrap.ts
@@ -1,3 +1,5 @@
+///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+
 import {bootstrap}    from 'angular2/platform/browser'
 import {provide}    from 'angular2/core'
 import {CORE_DIRECTIVES} from 'angular2/common'
@@ -9,4 +11,3 @@ bootstrap(AppComponent, [
     CORE_DIRECTIVES,
     provide(LocationStrategy, {useClass: PathLocationStrategy})
 ]);
-
